5c3ba0958e0d3678fe702daed6b39920949593a9
MartinaHuljaj <martina.huljaj@gmail.com>
2022-01-11 16:11:28 +0100

    Add project files.

---
+++ Aviation/DatabaseContext/AviationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AviationWebApi.DatabaseContext
+{
+    public class AviationContext: DbContext
+    {
+        public AviationContext(DbContextOptions<AviationContext> dbContextOptions)
+: base(dbContextOptions)
+        {
+        
+        }
+        public DbSet<Airport> Airports { get; set; }
+
+    }
+}
