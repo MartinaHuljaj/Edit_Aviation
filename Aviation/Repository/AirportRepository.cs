629bf5545584bb5d395ac1d9990bd5b14b5e9203
MartinaHuljaj <martina.huljaj@gmail.com>
2022-01-12 18:35:54 +0100

    GetAirports metoda

---
+++ Aviation/Repository/AirportRepository.cs
@@ -0,0 +1,26 @@
+﻿using AviationWebApi.DatabaseContext;
+using DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AviationWebApi.Repository
+{
+
+    public class AirportRepository
+    {
+        private readonly AviationContext _aviationContext;
+        public AirportRepository(AviationContext aviationContext)
+        {
+            _aviationContext = aviationContext;
+        }
+        public IEnumerable<Airport> GetAirports()
+        {
+            return _aviationContext.Airports.ToList();
+        }
+    }
+
+
+
+}
